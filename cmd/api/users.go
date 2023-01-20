8cc9b5b7a95a526a3edcf93f60ba141ad12cd1c7
Samat <samatabzalovich@gmail.com>
2023-01-21 01:32:19 +0600

    updated

---
+++ cmd/api/users.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"awesomeProject2/internal/data"
+	"awesomeProject2/internal/validator"
+	"errors"
+	"net/http"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	// Create an anonymous struct to hold the expected data from the request body.
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	// Parse the request body into the anonymous struct.
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	// Copy the data from the request body into a new User struct. Notice also that we
+	// set the Activated field to false, which isn't strictly necessary because the
+	// Activated field will have the zero-value of false by default. But setting this
+	// explicitly helps to make our intentions clear to anyone reading the code.
+	user := &data.User{
+		Name:      input.Name,
+		Email:     input.Email,
+		Activated: false,
+	}
+	// Use the Password.Set() method to generate and store the hashed and plaintext
+	// passwords.
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	v := validator.New()
+	// Validate the user struct and return the error messages to the client if any of
+	// the checks fail.
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	// Insert the user data into the database.
+	err = app.models.Users.Insert(user, r)
+	if err != nil {
+		switch {
+		// If we get a ErrDuplicateEmail error, use the v.AddError() method to manually
+		// add a message to the validator instance, and then call our
+		// failedValidationResponse() helper.
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Call the Send() method on our Mailer, passing in the user's email address,
+	// name of the template file, and the User struct containing the new user's data.
+	// Launch a goroutine which runs an anonymous function that sends the welcome email.
+	// Use the background helper to execute an anonymous function that sends the welcome
+	// email.
+	app.background(func() {
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+		}
+	})
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
